a6ff8651aa95fe0d38ed5a6b46133f13c43384c9
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-05 07:29:33 +0300

    [0.00.010] Add Less compiler BASH-file.

--- Builder/Build.sh
+++ Builder/Build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+bash Less.sh
 php ./Archiver.php
---
+++ Builder/Less.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "[Sources/Style]"; lessc ../Sources/Style.less ../Sources/Style.css;
+echo "[Sources/API/Areas/Show/Style]"; lessc ../Sources/API/Areas/Show/Style.less ../Sources/API/Areas/Show/Style.css;
