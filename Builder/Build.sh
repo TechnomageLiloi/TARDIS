2f4095b50e30b7168ad15005a0adcc219e177bc4
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-05 09:52:55 +0300

    [0.00.017] Update builder.

--- Builder/Build.sh
+++ Builder/Build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+echo "=-=-=-=-=-=-=-=-=-=";
+echo "= Building Boyard =";
+echo "=-=-=-=-=-=-=-=-=-=";
+
 bash Less.sh
+echo "Archiving in PHAR...";
 php ./Archiver.php
+echo "Done.";
--- Builder/Less.sh
+++ Builder/Less.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-echo "[Sources/Style]"; lessc ../Sources/Style.less ../Sources/Style.css;
-echo "[Sources/API/Areas/Show/Style]"; lessc ../Sources/API/Areas/Show/Style.less ../Sources/API/Areas/Show/Style.css;
+echo "Compiling LESS...";
+
+echo "Sources/Style"; lessc ../Sources/Style.less ../Sources/Style.css;
+echo "Sources/API/Areas/Show/Style"; lessc ../Sources/API/Areas/Show/Style.less ../Sources/API/Areas/Show/Style.css;
