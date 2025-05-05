e700ab8f8ea5fe9412fa22b04fc9970169c17134
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-05-05 04:55:51 +0300

    [0.00.004] Add Install block.

---
+++ Install/Database.sql
@@ -0,0 +1,17 @@
+create table logs
+(
+    key_log bigint unsigned auto_increment,
+    ts timestamp not null,
+    tags varchar(1000) not null,
+    data json not null,
+    constraint logs_pk
+        primary key (key_log)
+);
+
+create table config
+(
+    key_config varchar(100) not null,
+    data json not null,
+    constraint config_pk
+        primary key (key_config)
+);
