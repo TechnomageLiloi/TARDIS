3b3d3f099046fddbfe31b4976025e73a56f79265
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-06-24 18:13:29 +0300

    [0.04.002] Add euphoria JS requests.

---
+++ Engine/API/Euphoria/Requests.js
@@ -0,0 +1,61 @@
+Requests.Euphoria = {
+    show: function ()
+    {
+        API.request('Euphoria.Show', {
+            'debug': true
+        }, function (data) {
+            $('#layout').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    edit: function (key)
+    {
+        API.request('Euphoria.Edit', {
+            'key': key
+        }, function (data) {
+            $('#layout').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    save: function (key)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        const jq_block = $('#edit');
+        API.request('Euphoria.Save', {
+            key: key,
+            title: jq_block.find('[name=title]').val(),
+            price: jq_block.find('[name=price]').val(),
+            dt: jq_block.find('[name=dt]').val(),
+            summary: jq_block.find('[name=summary]').val(),
+            data: jq_block.find('[name=data]').val()
+        }, function (data) {
+            Requests.Euphoria.show();
+        }, function () {
+
+        });
+    },
+
+    create: function ()
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        API.request('Euphoria.Create', {
+            'debug': true
+        }, function (data) {
+            Requests.Euphoria.show();
+        }, function () {
+
+        });
+    },
+};
