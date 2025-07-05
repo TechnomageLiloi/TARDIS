b64ab129e45452ccb99e63d890ec1f5e5cb766be
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-07-05 06:51:34 +0300

    [0.06.014] Add active to ticket's status.

--- Engine/Domains/Tickets/Statuses.php
+++ Engine/Domains/Tickets/Statuses.php
@@ -4,13 +4,15 @@ namespace Liloi\TARDIS\Domains\Tickets;
 
 class Statuses
 {
-    public const TODO = 1;
+    public const TODO = 0;
+    public const ACTIVE = 1;
     public const IN_HAND = 2;
     public const SUCCESS = 3;
     public const FAILURE = 4;
 
     public static $list = [
         self::TODO => 'To Do',
+        self::ACTIVE => 'Active',
         self::IN_HAND => 'In hand',
         self::SUCCESS => 'Success',
         self::FAILURE => 'Failure',
