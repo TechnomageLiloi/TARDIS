d20d5f8fad950671d2133d9415007bb794bbb6b5
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-10-06 04:45:48 +0300

    [0.00.011] Update namespace.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TARDIS
+# UMKLAIDET
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/vendor/autoload.php';
 $private = json_decode(file_get_contents(__DIR__ . '/Config/Private.json'), true);
 
 $config = array_merge([
-    'title' => 'TARDIS',
+    'title' => 'UMKLAIDET',
     'start' => 'Requests.layout();',
     'scripts' => [
     ],
