e107c2be0e4b47a59c2de691427ca92a3d16885e
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-11-20 16:19:43 +0200

    [0.00.005] Add map types.

--- Engine/Domain/Maps/Types.php
+++ Engine/Domain/Maps/Types.php
@@ -5,8 +5,12 @@ namespace Liloi\TARDIS\Domain\Maps;
 class Types
 {
     public const UNTYPED = 1;
+    public const FOLDER = 2;
+    public const ARTICLE = 3;
 
     public static $list = [
         self::UNTYPED => 'Untyped',
+        self::FOLDER => 'Folder',
+        self::ARTICLE => 'Article',
     ];
 }
